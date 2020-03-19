39682f9007544c969344217ff074f43d5c140fc1
Alex-D <contact@alex-d.fr>
2020-03-19 18:44:22 +0100

    feat(core/config): detail type mismatch and escaped characters errors

--- monitorable/config/usecase/getconfig.go
+++ monitorable/config/usecase/getconfig.go
@@ -3,15 +3,25 @@ package usecase
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
+	"github.com/fatih/structs"
 	"github.com/monitoror/monitoror/monitorable/config/models"
 )
 
 var unknownFieldRegex *regexp.Regexp
+var fieldTypeMismatchRegex *regexp.Regexp
+var invalidEscapedCharacterRegex *regexp.Regexp
 
 func init() {
 	// Based on: https://github.com/golang/go/blob/release-branch.go1.14/src/encoding/json/decode.go#L755
 	unknownFieldRegex = regexp.MustCompile(`json: unknown field "(.*)"`)
+
+	// Based on: https://github.com/golang/go/blob/go1.14/src/encoding/json/scanner.go#L343
+	fieldTypeMismatchRegex = regexp.MustCompile(`json: cannot unmarshal .+ into Go struct field (.+) of type (.+)`)
+
+	// Based on: https://github.com/golang/go/blob/go1.14/src/encoding/json/scanner.go#L343
+	invalidEscapedCharacterRegex = regexp.MustCompile(`'(.*)' in string escape code`)
 }
 
 // GetConfig and set default value for Config from repository
@@ -56,12 +66,62 @@ func (cu *configUsecase) GetConfig(params *models.ConfigParams) *models.ConfigBa
 				field = subMatch[0][1]
 			}
 
+			var configField = structs.Fields(models.Config{})
+			var tileConfigFields = structs.Fields(models.TileConfig{})
+			var expectedFields = append(configField, tileConfigFields...)
+			var expectedFieldNames []string
+
+			for _, expectedField := range expectedFields {
+				var jsonTag = expectedField.Tag("json")
+				if jsonTag != "" && jsonTag != "-" {
+					var expectedFieldName = strings.Replace(jsonTag, ",omitempty", "", 1)
+					expectedFieldNames = append(expectedFieldNames, expectedFieldName)
+				}
+			}
+
 			configBag.AddErrors(models.ConfigError{
 				ID:      models.ConfigErrorUnknownField,
 				Message: e.Error(),
 				Data: models.ConfigErrorData{
 					FieldName:     field,
 					ConfigExtract: e.RawConfig,
+					Expected:      strings.Join(expectedFieldNames, ", "),
+				},
+			})
+		} else if fieldTypeMismatchRegex.MatchString(err.Error()) {
+			subMatch := fieldTypeMismatchRegex.FindAllStringSubmatch(err.Error(), 1)
+
+			var field = ""
+			var expectedType = ""
+			if len(subMatch) > 0 && len(subMatch[0]) > 1 {
+				fieldParts := strings.Split(subMatch[0][1], ".")
+				field = fieldParts[len(fieldParts)-1]
+				expectedType = subMatch[0][2]
+			}
+
+			configBag.AddErrors(models.ConfigError{
+				ID:      models.ConfigErrorFieldTypeMismatch,
+				Message: e.Error(),
+				Data: models.ConfigErrorData{
+					FieldName:     field,
+					ConfigExtract: e.RawConfig,
+					Expected:      expectedType,
+				},
+			})
+		} else if invalidEscapedCharacterRegex.MatchString(err.Error()) {
+			subMatch := invalidEscapedCharacterRegex.FindAllStringSubmatch(err.Error(), 1)
+
+			var invalidEscapedCharacter = ""
+			if len(subMatch) > 0 && len(subMatch[0]) > 1 {
+				invalidEscapedCharacter = subMatch[0][1]
+			}
+
+			configBag.AddErrors(models.ConfigError{
+				ID:      models.ConfigErrorInvalidEscapedCharacter,
+				Message: e.Error(),
+				Data: models.ConfigErrorData{
+					ConfigExtract:          e.RawConfig,
+					ConfigExtractHighlight: fmt.Sprintf(`\%s`, invalidEscapedCharacter),
 				},
 			})
 		} else {
